018362d17458d62e1c30a7f2161909c2ef8807d6
sneha1925vdp <55706754+sneha1925vdp@users.noreply.github.com>
2022-12-20 01:46:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,20 @@ import '../styles/App.css';
 class Odd extends React.Component {
 
   render() {
-    console.log("Even is unmounted");
+    console.log("Odd is unmounted");
     return (
       <div id="odd">
         I am odd
       </div>
     )
+    
   }
 }
 
 class Even extends React.Component {
 
   render() {
-    console.log("Odd is unmounted");
+    console.log("Even is unmounted");
     return (
       <div id="even">
         I am even
